11391c70f67786cfe4c33cfae4cff666d1469376
kitebeen <ypyp66@naver.com>
2021-10-06 01:05:14 +0900

    - /home으로 redirect -

--- src/Routes.js
+++ src/Routes.js
@@ -3,17 +3,19 @@ import {
   BrowserRouter as Router,
   Route,
   Switch,
+  Redirect,
 } from "react-router-dom";
+import BeerList from "./Pages/BeerList";
 import Home from "./Pages/Home";
-// import ReactGA from "react-ga";
 
 const Routes = () => {
-
   return (
     <Router>
-        <Switch>
-          <Route path="/home" component={Home} />
-        </Switch>
+      <Switch>
+        <Redirect exact from="/" to="/home" />
+        <Route path="/home" component={Home} />
+        <Route path="/beerlist" component={BeerList} />
+      </Switch>
     </Router>
   );
 };
